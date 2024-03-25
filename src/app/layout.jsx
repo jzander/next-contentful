11eb5d805a43b62dec848386c1822f3ccec01eda
Jeremy Zander <jeremyzander@Jeremys-MacBook-Pro-2.local>
2024-03-25 00:04:52 -0700

    meta data

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,23 +4,31 @@ import {fonts} from "@/app/fonts";
 import {TopBar} from "@/components/TopBar/TopBar";
 import {NavBar} from "@/components/NavbarWithPopover/NavbarWithPopover";
 import {Footer} from "@/components/FooterWithFourColumnsOnAccent/Footer";
-import {injectGTM} from "@/utils/gtm";
 import {client} from "../../lib/api";
+import {GoogleTagManager} from '@next/third-parties/google'
+import Head from "next/head";
 
 export default async function RootLayout({children}) {
     const global = await client.fetchContentfulGlobalDataGraphQL()
     const {globalData} = global
-    const gtmScript = globalData?.googleTagManagerId ? injectGTM(globalData.googleTagManagerId) : null;
     const data = {
         globalData: globalData,
         navigation: global.navigation,
         footerNavigation: global.footerNavigation,
-        gtmScript,
         logo: globalData.logo,
         phoneNumber: globalData.phoneNumber
     };
     return (
         <html lang='en'>
+        {globalData?.localBusinessMarkup &&
+            <Head>
+                <script type="application/ld+json"
+                        dangerouslySetInnerHTML={{__html: JSON.stringify(globalData.localBusinessMarkup)}}/>
+            </Head>
+        }
+        {globalData?.googleTagManagerId &&
+            <GoogleTagManager gtmId={globalData?.googleTagManagerId}/>
+        }
         <body className={fonts.rubik.variable} style={{background: "#fff"}}>
         <Providers>
             <TopBar {...data}/>
